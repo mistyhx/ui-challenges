273e61465df8bebb2967cd73de1d3e1b21ede617
mistyhx <hxmisty@gmail.com>
2019-11-04 12:52:07 -0500

    fixed the position of the card by removing padding

--- src/containers/CreditCardInfo/index.js
+++ src/containers/CreditCardInfo/index.js
@@ -13,8 +13,8 @@ function CreditCardInfo() {
 
   const props = useSpring({ opacity: 1, from: { opacity: 0 } });
   const { transform, opacity } = useSpring({
-    opacity: !flippedCard ? 1 : 0,
-    transform: `perspective(600px) rotateX(${!flippedCard ? -180 : 0}deg)`,
+    opacity: flippedCard ? 1 : 0,
+    transform: `perspective(600px) rotateY(${flippedCard ? -180 : 0}deg)`,
     config: { mass: 5, tension: 500, friction: 80 },
   });
 
@@ -29,38 +29,40 @@ function CreditCardInfo() {
     <div className="wrapper">
       <div className="card">
         <animated.div
-          className="card-front"
-          style={{ opacity, transform: transform.interpolate(t => `${t} rotateX(180deg)`) }}
+          className="card-back"
+          style={{ opacity, transform: transform.interpolate(t => `${t} rotateY(180deg)`) }}
         >
-          <div className="card__header">
-            <img
-              src="https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/chip.png"
-              className="card__chip"
-            />
-            <img
-              src="https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/visa.png"
-              className="card__type"
-            />
-          </div>
-
-          <div style={props} className="card__number">
-            {cardNumber ? cardNumber : "#### ##### ####"}
-          </div>
-          <div className="card__footer">
-            <div className="info">
-              <div className="label">Card Holder</div>
-              <animated.div style={props} className="value">
-                {cardHolder ? cardHolder : "Full Name"}
-              </animated.div>
-            </div>
-            <div className="info">
-              <div className="label"> Expired</div>
-              <div className="value">{expirationDate ? expirationDate : "MM/YY"}</div>
-            </div>
-          </div>
+          <div className="card-back-content"> Back of the card</div>
         </animated.div>
-        <animated.div className="card-back" style={{ opacity: opacity.interpolate(o => 1 - o), transform }}>
-          Back of the card
+        <animated.div className="card-front" style={{ opacity: opacity.interpolate(o => 1 - o), transform }}>
+          <div className="card-front-content">
+            <div className="card__header">
+              <img
+                src="https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/chip.png"
+                className="card__chip"
+              />
+              <img
+                src="https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/visa.png"
+                className="card__type"
+              />
+            </div>
+
+            <div style={props} className="card__number">
+              {cardNumber ? cardNumber : "#### ##### ####"}
+            </div>
+            <div className="card__footer">
+              <div className="info">
+                <div className="label">Card Holder</div>
+                <animated.div style={props} className="value">
+                  {cardHolder ? cardHolder : "Full Name"}
+                </animated.div>
+              </div>
+              <div className="info">
+                <div className="label"> Expired</div>
+                <div className="value">{expirationDate ? expirationDate : "MM/YY"}</div>
+              </div>
+            </div>
+          </div>
         </animated.div>
       </div>
       <div className="cardForm">
