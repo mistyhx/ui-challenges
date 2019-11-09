0bbec4c451142a5edcf8436439a0accc3dbefdf6
mistyhx <hxmisty@gmail.com>
2019-11-08 18:38:19 -0600

    added sliding in animation

--- src/containers/StackedCards/index.js
+++ src/containers/StackedCards/index.js
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
-import { useSpring, animated } from "react-spring";
+import { useTransition, animated } from "react-spring";
 import "./style.scss";
 
-const items = [
+const cities = [
   {
+    id: 1,
     title: "Chongqing",
     url: "https://en.cifnews.com/wp-content/uploads/2019/06/Chengdu-chongqing_cluster.jpg",
     date: "12.05.2019",
     length: 7,
   },
   {
+    id: 2,
     title: "Beijing",
     url:
       "https://cdn.contexttravel.com/image/upload/c_fill,q_60,w_2600/v1555942795/production/city/hero_image_22_1555942794.jpg",
@@ -17,12 +19,14 @@ const items = [
     length: 7,
   },
   {
+    id: 3,
     title: "Kunming",
     url: "https://pix10.agoda.net/geo/city/5445/1_5445_02.jpg?s=1920x822",
     date: "12.05.2019",
     length: 7,
   },
   {
+    id: 4,
     title: "Shanghai",
     url: "https://www.fodors.com/wp-content/uploads/2019/04/10UndertheRadarShanghai__HERO_shutterstock_351311348.jpg",
     date: "12.05.2019",
@@ -31,28 +35,38 @@ const items = [
 ];
 
 function StackedCards() {
+  const [items, setItems] = useState(cities);
+  const transitions = useTransition(items, item => item.id, {
+    from: { transform: "translate3d(0,200px,0)" },
+    enter: { transform: "translate3d(0,-100px,0)" },
+    trail: 200,
+  });
+
   return (
     <div className="card-deck">
-      {items.map((item, index) => (
-        <div
-          key={index}
-          className="card"
-          style={{
-            backgroundImage: `url(${item.url})`,
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            transform: `translateY(${index * 90}px)`,
-          }}
-        >
-          <div className="content">
-            <div className="info">
-              <div className="date">{item.date}</div>
-              <div className="length">{item.length} Days</div>{" "}
+      <h1>Travel Destinations</h1>
+      {transitions.map(({ item, props, id }) => (
+        <animated.div key={id} style={props}>
+          <div
+            key={item.id}
+            className="card"
+            style={{
+              backgroundImage: `url(${item.url})`,
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+              transform: `translateY(${item.id * 90}px)`,
+            }}
+          >
+            <div className="content">
+              <div className="info">
+                <div className="date">{item.date}</div>
+                <div className="length">{item.length} Days</div>{" "}
+              </div>
+              <div className="title">{item.title}</div>
             </div>
-            <div className="title">{item.title}</div>
           </div>
-        </div>
+        </animated.div>
       ))}
     </div>
   );
